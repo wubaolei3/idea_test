45705600034e8abedb23487f0d0dd68c1bd9f892
wubaolei <wubaolei3@163.com>
2019-03-17 14:02:38 +0800

    1

--- manager-web/src/main/java/com/wbl/web/controller/PageController.java
+++ manager-web/src/main/java/com/wbl/web/controller/PageController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
+
     @RequestMapping("/{page}")
     public String showPage(@PathVariable String page){
+        System.out.println("s");
         return page;
     }
 }
